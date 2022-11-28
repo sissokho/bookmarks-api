825aa31ed7ae0a050044990f8e7700e849d81021
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-11-28 11:07:42 +0000

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Model::preventLazyLoading(! $this->app->isProduction());
+
+        Model::preventAccessingMissingAttributes(! $this->app->isProduction());
+
+        if ($this->app->environment('local')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
