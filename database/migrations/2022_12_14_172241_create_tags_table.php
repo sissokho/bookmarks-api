730b26292827cb5648c65fabcdc1f3fa2a72b631
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2023-06-12 13:14:44 +0000

    DB Schema: Tags are not managed by users

--- database/migrations/2022_12_14_172241_create_tags_table.php
+++ database/migrations/2022_12_14_172241_create_tags_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
 
-            $table->string('name');
-            $table->string('slug');
-
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
 
             $table->timestamps();
-
-            $table->unique(['slug', 'user_id']);
         });
     }
 
--- tests/Feature/Tags/IndexTest.php
+++ tests/Feature/Tags/IndexTest.php
@@ -14,20 +14,27 @@ class IndexTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function users_tags_are_listed_by_the_most_recent_in_a_paginated_way(): void
+    public function unauthenticated_user_cannot_see_the_list_of_tags(): void
     {
-        $user = User::factory()->create();
+        $response = $this->getJson(route('api.v1.tags.index'));
 
-        Sanctum::actingAs($user);
+        $response->assertUnauthorized()
+            ->assertExactJson([
+                'message' => 'Unauthenticated.',
+            ]);
+    }
+
+    /** @test */
+    public function tags_are_listed_by_the_most_recent_in_a_paginated_way(): void
+    {
+        Sanctum::actingAs(User::factory()->make());
 
         Tag::factory()
             ->count(20)
-            ->for($user)
             ->state(['created_at' => now()->subDay()])
             ->create();
 
         $mostRecentTag = Tag::factory()
-            ->for($user)
             ->create();
 
         $response = $this->getJson(route('api.v1.tags.index'));
@@ -39,9 +46,9 @@ class IndexTest extends TestCase
                     ->has(
                         'data.0',
                         fn ($json) => $json->where('id', $mostRecentTag->id)
-                            ->where('name', strtolower($mostRecentTag->name))
+                            ->where('name', $mostRecentTag->name)
                             ->where('slug', $mostRecentTag->slug)
-                            ->where('created_at', $mostRecentTag->created_at->toJson())
+                            ->where('created_at', $mostRecentTag->created_at->toDateTimeString())
                     )
             )
             ->assertJson([
@@ -63,26 +70,12 @@ class IndexTest extends TestCase
     }
 
     /** @test */
-    public function unauthenticated_user_cannot_see_the_list_of_tags(): void
-    {
-        $response = $this->getJson(route('api.v1.tags.index'));
-
-        $response->assertUnauthorized()
-            ->assertExactJson([
-                'message' => 'Unauthenticated.',
-            ]);
-    }
-
-    /** @test */
     public function user_can_customize_the_pagination(): void
     {
-        $user = User::factory()->create();
-
-        Sanctum::actingAs($user);
+        Sanctum::actingAs(User::factory()->make());
 
         Tag::factory()
             ->count(20)
-            ->for($user)
             ->create();
 
         $response = $this->getJson(route('api.v1.tags.index', ['per_page' => 10, 'page' => 2]));
@@ -114,91 +107,66 @@ class IndexTest extends TestCase
      * @test
      * @dataProvider invalidPaginationParameters
      */
-    public function pagination_parameters_are_validated($invalidParameters, $errors): void
+    public function pagination_parameters_are_validated($payload, $errors): void
     {
         Sanctum::actingAs(User::factory()->make());
 
-        $response = $this->getJson(route('api.v1.tags.index', $invalidParameters));
+        $response = $this->getJson(route('api.v1.tags.index', $payload));
 
         $response->assertUnprocessable()
             ->assertInvalid($errors);
-
-        // $response = $this->getJson(route('api.v1.tags.index', [
-        //     'page' => '5ju',
-        //     'per_page' => '6fd'
-        // ]));
-
-        // $response->assertUnprocessable()
-        //     ->assertInvalid([
-        //         'per_page' => [
-        //             'The per page must be a number.',
-        //             'The per page must be an integer.'
-        //         ],
-        //         'page' => [
-        //             'The page must be a number.',
-        //             'The page must be an integer.'
-        //         ],
-        //     ]);
-
-        // $response = $this->getJson(route('api.v1.tags.index', [
-        //     'page' => 5.2,
-        //     'per_page' => 4.5
-        // ]));
-
-        // $response->assertUnprocessable()
-        //     ->assertInvalid([
-        //         'per_page' => 'The per page must be an integer.',
-        //         'page' => 'The page must be an integer.',
-        //     ]);
-
-        // $response = $this->getJson(route('api.v1.tags.index', [
-        //     'page' => -5,
-        //     'per_page' => -2
-        // ]));
-
-        // $response->assertUnprocessable()
-        //     ->assertInvalid([
-        //         'per_page' => 'The per page must be at least 1.',
-        //         'page' => 'The page must be at least 1.',
-        //     ]);
-
-        // $response = $this->getJson(route('api.v1.tags.index', [
-        //     'per_page' => 101
-        // ]));
-
-        // $response->assertUnprocessable()
-        //     ->assertInvalid([
-        //         'per_page' => 'The per page must not be greater than 100.'
-        //     ]);
     }
 
     public function invalidPaginationParameters(): array
     {
         return [
-            'per & per_page are strings' => [['page' => '5ju', 'per_page' => '6fd'], [
-                'per_page' => [
-                    'The per page must be a number.',
-                    'The per page must be an integer.',
+            'per & per_page are strings' => [
+                'payload' => [
+                    'page' => '5ju',
+                    'per_page' => '6fd',
                 ],
-                'page' => [
-                    'The page must be a number.',
-                    'The page must be an integer.',
+                'errors' => [
+                    'per_page' => [
+                        'The per page must be a number.',
+                        'The per page must be an integer.',
+                    ],
+                    'page' => [
+                        'The page must be a number.',
+                        'The page must be an integer.',
+                    ],
                 ],
-            ]],
+            ],
 
-            'per & per_page are floats' => [['page' => 5.2, 'per_page' => 4.5], [
-                'per_page' => 'The per page must be an integer.',
-                'page' => 'The page must be an integer.',
-            ]],
+            'per & per_page are floats' => [
+                'payload' => [
+                    'page' => 5.2,
+                    'per_page' => 4.5,
+                ],
+                'errors' => [
+                    'per_page' => 'The per page must be an integer.',
+                    'page' => 'The page must be an integer.',
+                ],
+            ],
 
-            'per & per_page are less than 1' => [['page' => -5, 'per_page' => -2], [
-                'per_page' => 'The per page must be at least 1.',
-                'page' => 'The page must be at least 1.',
-            ]],
+            'per & per_page are less than 1' => [
+                'payload' => [
+                    'page' => -5,
+                    'per_page' => -2,
+                ],
+                'errors' => [
+                    'per_page' => 'The per page must be at least 1.',
+                    'page' => 'The page must be at least 1.',
+                ],
+            ],
 
-            'per_page is greater than 100' => [['per_page' => 101], [
-                'per_page' => 'The per page must not be greater than 100.',
-            ]],
+            'per_page is greater than 100' => [
+                'payload' => [
+                    'per_page' => 101,
+                ],
+                'errors' => [
+                    'per_page' => 'The per page must not be greater than 100.',
+                ],
+            ],
         ];
     }
 }
