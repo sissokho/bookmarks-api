285a43a485e00294d9639e67fd59780ca85d7799
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2023-07-09 13:42:57 +0000

    Fix phpstan error: inline request->user() and use nullsafe operator

--- app/Http/Controllers/Api/V1/Bookmarks/StoreController.php
+++ app/Http/Controllers/Api/V1/Bookmarks/StoreController.php
@@ -6,17 +6,13 @@ use App\Actions\FetchOrCreateTags;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Bookmarks\StoreRequest;
 use App\Http\Resources\V1\BookmarkResource;
-use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request, FetchOrCreateTags $fetchOrCreateTags): JsonResource
     {
-        /** @var User $user */
-        $user = $request->user();
-
-        $bookmark = $user->bookmarks()->create(
+        $bookmark = $request->user()?->bookmarks()->create(
             $request->safe()->except('tags')
         );
 
